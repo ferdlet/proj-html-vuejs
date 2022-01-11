30b8060375e0be69c6511eb044c6e30565eaf915
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-01-11 14:41:28 +0100

    menu layout

--- src/components/sections/Menu.vue
+++ src/components/sections/Menu.vue
@@ -1,8 +1,34 @@
 <template>
     <section id="ham-menu">
-        <div class="close-btn" v-on:click="dataShared.hamMenuOpen = false">
-            <i class="fas fa-times"></i>
+        <div class="header-menu">
+            <img class="logo-img" src="../../assets/images/avada-music-logo.png" alt="Avada Music">
+            <div class="close-btn" v-on:click="dataShared.hamMenuOpen = false">
+                <i class="fas fa-times"></i>
+            </div>
         </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+            </ul>
+        </nav>
     </section>
 </template>
 
@@ -34,5 +60,36 @@ export default {
             color: $white;
             font-size: 3rem;
         }
+        .header-menu {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 30px 50px;
+            .logo-img {
+                height: 80px;
+            }
+        }
+        nav {
+            height: calc(100vh - 140px);
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            ul {
+                list-style: none;
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+                li {
+                    margin-bottom: 50px;
+                    a {
+                        font-size: 36px;
+                        text-decoration: none;
+                        color: $white;
+                    }
+                }
+            }
+        }
     }
+
+
 </style>
